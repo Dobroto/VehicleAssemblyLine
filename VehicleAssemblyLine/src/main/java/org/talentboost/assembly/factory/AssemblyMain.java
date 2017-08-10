cdd88b7130211faf832fcf8cfc99f8f046e1e651
Borislav Georgiev <georg.borislav@gmail.com>
2017-08-10 16:49:09 +0300

    Validated country code and factory number

--- VehicleAssemblyLine/src/main/java/org/talentboost/assembly/factory/AssemblyMain.java
+++ VehicleAssemblyLine/src/main/java/org/talentboost/assembly/factory/AssemblyMain.java
@@ -3,34 +3,48 @@ package org.talentboost.assembly.factory;
 import java.util.Scanner;
 
 import org.talentboost.assembly.validators.CountryValidator;
+import org.talentboost.assembly.validators.FactoryNumberValidator;
 
 public class AssemblyMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter the name of the factory's country.");
+		System.out.println("Please enter the factory's country ISO code.");
 		Scanner sc = new Scanner(System.in);
 		CountryValidator cv = new CountryValidator();
+		FactoryNumberValidator fnv = new FactoryNumberValidator();
+		String validCountryCode = null;
+		String validFactoryNum = null;
 		
 		try {
     		while(sc.hasNextLine()) {
-    			if (!cv.validate(sc.nextLine().trim())) {
-    				System.out.println("Please enter valid country name.");
+    			validCountryCode = sc.nextLine().trim();
+    			if (!cv.validate(validCountryCode)) {
+    				System.out.println("Please enter valid country ISO code.");
     			}
     			else {
     				break;
     			}
     		}
+    		
+    		System.out.println("Please enter your factory's number.");
+    		
+    		while(sc.hasNextLine()) {
+    			validFactoryNum = sc.nextLine().trim();
+    			if (!fnv.validate(validFactoryNum)) {
+    				System.out.println("Please enter a number from 0 to 9.");
+    			}
+    			else {
+    				break;
+    			}
+    		}
+    		
     	}
     	finally {
     		sc.close();
     	}
 		
-		
-		
-		
-		
-		Factory factory = new Factory();
+		Factory factory = new Factory(validCountryCode, validFactoryNum);
 		System.out.println("Please put the commands files in your home directory.");
 		
 
--- VehicleAssemblyLine/src/main/java/org/talentboost/assembly/factory/Factory.java
+++ VehicleAssemblyLine/src/main/java/org/talentboost/assembly/factory/Factory.java
@@ -1,7 +1,13 @@
 package org.talentboost.assembly.factory;
 
 public final class Factory {
+	private String countryCode;
+	private String factoryNum;
 	
+	public Factory(String countryCode, String factoryNum) {
+		this.countryCode = countryCode;
+		this.factoryNum = factoryNum;
+	}
 	public void initialiseFactory() {
 		
 	}
